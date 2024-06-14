62d8e50fe6f017f027c824abda02c37db96fd5bd
simsol180 <34282063+simsol180@users.noreply.github.com>
2024-06-14 09:53:00 -0400

    basic-start
    added security helper so we can encrypt and hash
    started api controller to listen to /login
    created excel session to handle login logic

---
+++ wish-well-1/Controllers/API/Session.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json.Serialization;
+using wish_well_1.Controllers.Excel;
+
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace wish_well_1.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Session : ControllerBase
+    {
+
+        // POST api/<Session>
+        [HttpPost("/login")]
+        public void Post([FromBody] string email, [FromBody] string password) {
+            
+        }
+
+    }
+}
---
+++ wish-well-1/Controllers/Excel/ExcelSession.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Reflection.Metadata.Ecma335;
+
+namespace wish_well_1.Controllers.Excel
+{
+    public class ExcelSession
+    {
+        private string EncryptEmail(string email) {
+            return "";
+
+        }
+        private string HashPassword(string password) {
+            return "";
+        }
+        private bool TryLogin(string email, string password) {
+            var hashedPassword = HashPassword(password);
+            var encryptedEmail = EncryptEmail(email);
+
+            return false;
+        }
+        public bool Login([FromBody] string email, [FromBody] string password) {
+            if (string.IsNullOrEmpty(email) || string.IsNullOrEmpty(password)) {
+                return false;
+            }
+ 
+            return TryLogin(email,password);
+        }
+    }
+}
