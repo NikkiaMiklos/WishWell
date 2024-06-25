51033183fe9ef27c5350ed3862ba2f647630c359
Nikkia Bush <nikkiamiklos@gmail.com>
2024-06-24 20:23:02 -0400

    I did html and css

---
+++ wish-well-1/ClientApp/src/app/fetch-data/productsfFetch.ts
@@ -0,0 +1,23 @@
+import { Component, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-fetch-data',
+  templateUrl: './fetch-data.component.html'
+})
+export class FetchDataComponent {
+  public Product: Product[] = [];
+
+  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
+    http.get<Product[]>(baseUrl + 'weatherforecast').subscribe(result => {
+      this.products = result;
+    }, error => console.error(error));
+  }
+}
+
+interface Product {
+  Name: string;
+  Url: URL;
+  Price: number;
+  UserID: string;
+}
