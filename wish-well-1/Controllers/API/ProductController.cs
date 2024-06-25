6cf80fa1d82c72abe9705b77870710fae44737b0
Miklosam <41344657+Miklosam@users.noreply.github.com>
2024-06-24 21:17:03 -0400

    products

---
+++ wish-well-1/Controllers/API/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace wish_well_1.Controllers
+{
+    [ApiController]
+    [Route("product")]
+    public class ProductController : Controller
+    {
+        private ProductsCsvController _ProductsCsvController = new ProductsCsvController();
+
+        [HttpPost("create")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([FreeBody] Product product)
+        {
+            int productID = int.IsNullOrEmpty(product.ID) ? "" : product.ID;
+            string name = string.IsNullOrEmpty(product.Name) ? "" : product.Name;
+            string url = string.IsNullOrEmpty(product.Url) ? "" : product.Url;
+            string price = string.IsNullOrEmpty(product.Price) ? "" : product.Price;
+            int userId = int.IsNullOrEmpty(product.UserId) ? "" : product.UserId;
+
+            return Ok(_ProductsCsvController.addProductToList(productID, name, url, price, userId));
+        }
+    }
+}
---
+++ wish-well-1/Controllers/Csv/ProductsCsvController.cs
@@ -0,0 +1,34 @@
+﻿using System.Xml.Linq;
+using System;
+
+namespace wish_well_1.Controllers
+{
+    public class ProductsCsvController
+    {
+        public bool checkIfProductExists(int productId)
+        {
+            var productExists = false;
+            CsvController.ReadEachProductByLambda((Product Product) => {
+                if (Product.ID == productId)
+                {
+                    productExists = true;
+                }
+                else 
+                {
+                    productExists = false;
+                }
+            });
+            return productExists;
+        }
+        public bool addProductToList(int productId, string Url, string Name, string Price, int userID ) {
+            if (!checkIfProductExists(productId))
+            {
+                return CsvController.AddProduct(Url, Name, Price, userID);
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
